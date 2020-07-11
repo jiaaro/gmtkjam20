3cf8c734bf5a14db7e0b09aa598676db3379c7e6
James Robert <me@jiaaro.com>
2020-07-11 14:32:31 -0500

    ttl in seconds

--- src/projectile.lua
+++ src/projectile.lua
@@ -21,7 +21,7 @@ function Projectile:update(dt)
       self.x + self.velocity.x * dt,
       self.y + self.velocity.y * dt
     )
-    self.ttl = self.ttl - 1
+    self.ttl = self.ttl - dt
   else
     self.active = false
     self.velocity = {x = 0, y = 0}
@@ -45,7 +45,7 @@ function Projectile:start(startX, startY, velocityX, velocityY)
   self.y = startY
   self.velocity = {x = velocityX, y = velocityY}
   self.active = true
-  self.ttl = 60
+  self.ttl = 1
 end
 
 return Projectile
